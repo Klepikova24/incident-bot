adfc0cec3fb99665402d6723cc74dc8ad98427a8
Klepikova24 <eglettt980@gmail.com>
2026-01-31 00:31:14 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const TelegramBot = require('node-telegram-bot-api');
+const cron = require('node-cron');
+
+// ВАЖНО: Вставьте ваш токен от @BotFather между кавычками!
+const token = 'ВАШ_ТОКЕН_ЗДЕСЬ';
+const bot = new TelegramBot(token, { polling: true });
+
+let incidentCounters = {}; // Хранилище счетчиков (временное)
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id.toString();
+  // ... (код инициализации и отправки сообщения с кнопкой)
+});
+
+bot.on('callback_query', (callbackQuery) => {
+  // ... (код обработки нажатия кнопки "Произошел инцидент")
+});
+
+cron.schedule('0 12 * * *', () => {
+  // ... (код для ежедневного увеличения счетчика и отправки отчета)
+}, {
+  timezone: "UTC"
+});
+
+console.log('Бот запущен!');
